d875d4f7dce59db5ad34c1325dd3d720aee7b963
benpolletta <benpolletta@gmail.com>
2022-11-15 15:12:42 -0500

    Edits to files for plotting PAC.

--- AllPowByThetaPlots.m
+++ AllPowByThetaPlots.m
@@ -19,12 +19,12 @@ end
 
 if nargin < 3, frequencies = []; end
 if isempty(frequencies)
-    frequencies = {9:60, 9:40};
+    frequencies = {9:100, 9:40}; %{9:60, 9:40};
 end
 
 if nargin < 4, cycle_cell = []; end
 if isempty(cycle_cell)
-    cycle_cell={4, linspace(4,12,52), 6, linspace(6,18,52), 8, linspace(8,24,52)};
+    cycle_cell={4, linspace(4,18,92), 6, linspace(6,24,92), 8, linspace(8,30,92)}; %{4, linspace(4,12,52), 6, linspace(6,18,52), 8, linspace(8,24,52)};
 end
 
 for p = 1:length(prefixes)
@@ -40,7 +40,7 @@ for p = 1:length(prefixes)
 
             label = sprintf('%s_%.2gto%.2gHz_%.2gto%.2gcyc', datafile, min(freqs), max(freqs), min(no_cycles), max(no_cycles))
 
-            if exist([label, '.mat'], 'file')
+            if exist([pwd, '/', label, '.mat'], 'file')
 
                 datamat = load([label, '.mat']);
                 ws = datamat.ws;
@@ -50,16 +50,16 @@ for p = 1:length(prefixes)
                 data = load(datafile);
                 time = (1:length(data))/fs;
                 ws = wavelet_spectrogram(data, fs, freqs, no_cycles, 1, label);
-                save(label, 'data', 'time', 'fs', 'freqs', 'no_cycles', 'ws', '-v7.3')
+                save([pwd, '/', label], 'data', 'time', 'fs', 'freqs', 'no_cycles', 'ws', '-v7.3')
 
             end
 
             time = (1:size(ws, 1))/fs;
 
-            no_thetas = max(time)/.25;
+            no_thetas = floor(max(time)/.25);
             length_theta = .25*fs;
 
-            ws_folded = reshape(ws, [length_theta, no_thetas, length(freqs)]);
+            ws_folded = reshape(ws(1:(no_thetas*length_theta), :), [length_theta, no_thetas, length(freqs)]);
             ws_tmean = squeeze(nanmean(abs(ws_folded), 2));
 
             figure(2*p-1)
--- SelectedPowByThetaPlots.m
+++ SelectedPowByThetaPlots.m
@@ -7,20 +7,20 @@ function SelectedPowByThetaPlots
 
 fs = 10^5;
 
-prefixes = {'LIP', 'FEFvm'};
+prefixes = {'mean'}; % {'LIP'}; %, 'FEFvm'};
 
-frequencies = {9:60, 9:40};
+frequencies = {9:100}; % {9:100, 9:40};
 
-suffix = '_LFP_V_RS';
+suffixes = {'_V_RS.txt'}; % {'_LFP_V_RS_mixed'}; %, 'LFP_V_RS'};
 
-all_cycles = linspace(4,12,52);
+all_cycles = linspace(4,18,92);
 
-for p = 1:2
+for p = 1:length(prefixes)
     
     freqs = frequencies{p};
     cycles = all_cycles(1:min(length(all_cycles),length(freqs)));
     
-    datafile = [prefixes{p}, suffix];
+    datafile = [prefixes{p}, suffixes{p}];
     
     label = sprintf('%s_%.2gto%.2gHz_%.2gto%.2gcyc', datafile, min(freqs), max(freqs), min(cycles), max(cycles))
     
@@ -34,23 +34,23 @@ for p = 1:2
         data = load(datafile);
         time = (1:length(data))/fs;
         ws = wavelet_spectrogram(data, fs, freqs, cycles, 1, label);
-        save(label, 'data', 'time', 'fs', 'freqs', 'no_cycles', 'ws', '-v7.3')
+        save(label, 'data', 'time', 'fs', 'freqs', 'all_cycles', 'ws', '-v7.3')
         
     end
     
     time = (1:size(ws, 1))/fs;
     
-    no_thetas = max(time)/.25;
+    no_thetas = floor((max(time)-.25)/.25);
     length_theta = .25*fs;
     theta_phase = 360*time(1:length_theta)/max(time(1:length_theta));
     
-    ws_folded = reshape(ws, [length_theta, no_thetas, length(freqs)]);
+    ws_folded = reshape(ws((1:no_thetas)*length_theta, :), [length_theta, no_thetas, length(freqs)]);
     ws_tmean = squeeze(nanmean(abs(ws_folded), 2));
     
     ws_mean_mat = ones(size(ws_tmean))*diag(nanmean(ws_tmean));
     ws_pct_tmean = (ws_tmean - ws_mean_mat)./ws_mean_mat;
     
-    subplot(2, 1, p)
+    subplot(length(prefixes), 1, p)
     imagesc(theta_phase, freqs, ws_pct_tmean')
     axis xy
     set(gca, 'FontSize', 20)
